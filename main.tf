d2bfa7c68b7c807cbfebea26240dc0353eef4acd
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-11 09:41:36 +0200

    remove backend configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 terraform {
   required_version = "0.12.9"
-
-  # backend "s3" {
-  #   key            = "terraform.tfstate"
-  #   bucket         = "ied.infra.terraform"
-  #   profile        = "terraform"
-  #   region         = "eu-central-1"
-  #   dynamodb_table = "ied.terraform-lock"
-  # }
 }
